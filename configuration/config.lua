1daef22436b39d7768740820c6a6b0f57df6eab4
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-11-17 00:32:19 -0800

    refactor: bunch

---
+++ configuration/config.lua
@@ -0,0 +1,57 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+
+Config = {}
+
+Config.checkForUpdates = true -- Check for Updates?
+Config.oldESX = false --Getting 'canCarryItem' error? Set this to true if you're using limit system.
+
+Config.axe = {
+    prop = `prop_tool_pickaxe`, --Default: `prop_tool_pickaxe`
+    breakChance = 20 -- When failing to mine rock, this is the percentage of a chance that your pickaxe will 'break'
+}
+
+Config.rocks = { -- Items obtained from mining
+    { item = 'emerald', price = {190, 220}, difficulty = {'medium', 'medium', 'easy'} },
+    { item = 'diamond', price = {150, 180}, difficulty = {'medium', 'easy', 'easy'} },
+    { item = 'copper', price = {110, 140}, difficulty = {'medium', 'easy'} },
+    { item = 'iron', price = {70, 100}, difficulty = {'easy', 'easy'} },
+    { item = 'steel', price = {40, 60}, difficulty = {'easy', 'easy'} },
+}
+
+
+Config.miningAreas = {
+    vec3(2977.45, 2741.62, 44.62), -- vec3 of locations for mining stones
+    vec3(2982.64, 2750.89, 42.99),
+    vec3(2994.92, 2750.43, 44.04),
+    vec3(2958.21, 2725.44, 50.16),
+    vec3(2946.3, 2725.36, 47.94),
+    vec3(3004.01, 2763.27, 43.56),
+    vec3(3001.79, 2791.01, 44.82)
+}
+
+Config.sellShop = {
+    enabled = true, -- Enable spot to sell the things mined?
+    coords = vec3(122.1, 6405.69, 31.36-0.9), -- Location of buyer
+    heading = 314.65, -- Heading of ped
+    ped = 'cs_joeminuteman' -- Ped name here
+}
+
+RegisterNetEvent('wasabi_mining:notify')
+AddEventHandler('wasabi_mining:notify', function(title, message, msgType)	
+    -- Place notification system info here, ex: exports['mythic_notify']:SendAlert('inform', message)
+    if not msgType then
+        lib.notify({
+            title = title,
+            description = message,
+            type = 'inform'
+        })
+    else
+        lib.notify({
+            title = title,
+            description = message,
+            type = msgType
+        })
+    end
+end)
---
+++ configuration/strings.lua
@@ -0,0 +1,33 @@
+Strings = {
+    --Blips
+    mining_blips = 'Mine',
+    sell_shop_blip = 'Mining Sales',
+
+    --Help Text
+    intro_instruction = 'Press ~INPUT_ATTACK~ to mine rocks, ~INPUT_FRONTEND_RRIGHT~ to cancel.',
+
+    --Text UI
+    mine_rock = '[E] - Mine Rock',
+    sell_material = '[E] - Sell Material',
+
+    --Notifications(Success)
+    rewarded = 'Mined Rock',
+    rewarded_desc = 'You have successfully mined 1x',
+    sold_for = 'Items Sold',
+    sold_for_desc = 'You have sold %sx %s for $%s',
+
+    --Notifications(Failed)
+    failed_mine = 'Failed',
+    failed_mine_desc = 'You failed to mine rock!',
+    no_pickaxe = 'No Pickaxe',
+    no_pickaxe_desc = 'You do not have a pickaxe!',
+    axe_broke = 'Axe Broke',
+    axe_broke_desc = 'You hit the rock wrong and your axe snapped!',
+    cantcarry = 'Can\'t Carry',
+    cantcarry_desc = 'No more room to carry 1x',
+    possible_cheater = 'Exploit Detected',
+    possible_cheater_desc = 'You have been reported to staff.',
+
+    --Kicked Message
+    kicked = 'You have been kicked and reported to staff for possible cheating.'
+}
