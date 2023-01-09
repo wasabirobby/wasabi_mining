962ec7184d36d8f8a19277c9cca8b1d557902bec
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 18:47:25 -0800

    chore: formatting

--- client/functions.lua
+++ client/functions.lua
@@ -1,14 +1,13 @@
 -----------------For support, scripts, and more----------------
 --------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
-
 showHelp = function(msg)
     BeginTextCommandDisplayHelp('STRING')
     AddTextComponentSubstringPlayerName(msg)
     EndTextCommandDisplayHelp(0, false, true, -1)
 end
 
-createBlip = function(coords, sprite, colour, text, scale)
+CreateBlip = function(coords, sprite, colour, text, scale)
     local blip = AddBlipForCoord(coords)
     SetBlipSprite(blip, sprite)
     SetBlipColour(blip, colour)
