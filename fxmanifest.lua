3d58ef6891e293fd67b60d82d7856a7063cc9222
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 18:54:53 -0800

    chore: update manifest/version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,28 +3,17 @@
 ---------------------------------------------------------------
 
 fx_version 'cerulean'
-use_experimental_fxv2_oal 'yes'
 game 'gta5'
 lua54 'yes'
 
-version '2.0.0'
+version '2.0.1'
 author 'wasabirobby#5110'
-description 'A simple yet smooth ESX based mining script'
+description 'A simple yet smooth ESX/QBCore based mining script'
 
-shared_scripts {
-    '@ox_lib/init.lua',
-    'configuration/*.lua'
-}
+shared_scripts { '@ox_lib/init.lua', 'configuration/*.lua' }
 
-client_scripts {
-    'client/*.lua'
-}
+client_scripts { 'bridge/**/client.lua', 'client/*.lua' }
 
-server_scripts {
-    'server/*.lua'
-}
+server_scripts { 'bridge/**/server.lua', 'server/*.lua' }
 
-dependencies {
-    'es_extended',
-    'ox_lib'
-}
+dependencies { 'ox_lib' }
