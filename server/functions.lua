6db4a8f424d3b9f55c2cee883dc360e90d485995
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 18:51:36 -0800

    Delete functions.lua

--- server/functions.lua
+++
@@ -1,8 +0,0 @@
------------------For support, scripts, and more----------------
---------------- https://discord.gg/wasabiscripts  -------------
----------------------------------------------------------------
-
-addCommas = function(n)
-	return tostring(math.floor(n)):reverse():gsub('(%d%d%d)','%1,')
-								  :gsub(',(%-?)$','%1'):reverse()
-end
