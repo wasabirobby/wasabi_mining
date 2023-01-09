5eac49600ddb458f1934f8afa1d1d229f1a651b4
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 18:51:24 -0800

    feat: qbcore support

--- server/server.lua
+++ server/server.lua
@@ -2,11 +2,8 @@
 --------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 
-ESX = Config.esxImport()
-
 lib.callback.register('wasabi_mining:checkPick', function(source, itemname)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local item = xPlayer.getInventoryItem(itemname).count
+    local item = HasItem(source, itemname)
     if item >= 1 then
         return true
     else
@@ -19,73 +16,61 @@ lib.callback.register('wasabi_mining:getRockData', function(source)
     return data
 end)
 
-RegisterNetEvent('wasabi_mining:mineRock', function(data, index)
-    local xPlayer = ESX.GetPlayerFromId(source)
+local addCommas = function(n)
+	return tostring(math.floor(n)):reverse():gsub("(%d%d%d)","%1,")
+								  :gsub(",(%-?)$","%1"):reverse()
+end
+
+RegisterServerEvent("wasabi_mining:mineRock")
+AddEventHandler("wasabi_mining:mineRock", function(data, index)
     local playerPed = GetPlayerPed(source)
     local playerCoord = GetEntityCoords(playerPed)
     local distance = #(playerCoord - Config.miningAreas[index])
     if distance == nil then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
         return
     end
     if distance > 10 then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(Strings.kicked)
         return
     end
-    local awardItem = data.item
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local awardItemLabel = ESX.GetItemLabel(awardItem)
-    if OldESX then
-        local limitItem = xPlayer.getInventoryItem(awardItem)
-        if limitItem.limit == -1 or (limitItem.count + 1) <= limitItem.limit then
-            xPlayer.addInventoryItem(awardItem, 1)
-            TriggerClientEvent('wasabi_mining:notify', source, Strings.rewarded, Strings.rewarded_desc..' '..awardItemLabel, 'success')
-        else
-            TriggerClientEvent('wasabi_mining:notify', source, Strings.cantcarry, Strings.cantcarry_desc..' '..awardItemLabel, 'error')
-        end
-    else
-        if xPlayer.canCarryItem(awardItem, 1) then
-            xPlayer.addInventoryItem(awardItem, 1)
-            TriggerClientEvent('wasabi_mining:notify', source, Strings.rewarded, Strings.rewarded_desc..' '..awardItemLabel, 'success')
-        else
-            TriggerClientEvent('wasabi_mining:notify', source, Strings.cantcarry, Strings.cantcarry_desc..' '..awardItemLabel, 'error')
-        end
-    end
+    AddItem(source, data.item, 1)
+    TriggerClientEvent('wasabi_mining:notify', source, Strings.rewarded, Strings.rewarded_desc..' '..data.label, 'success')
 end)
 
-RegisterNetEvent('wasabi_mining:sellRock', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
+RegisterServerEvent('wasabi_mining:sellRock')
+AddEventHandler('wasabi_mining:sellRock', function()
     local playerPed = GetPlayerPed(source)
     local playerCoord = GetEntityCoords(playerPed)
     local distance = #(playerCoord - Config.sellShop.coords)
     if distance == nil then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
         return
     end
     if distance > 3 then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
         return
     end
     for i=1, #Config.rocks do
-        if xPlayer.getInventoryItem(Config.rocks[i].item).count then
+        if HasItem(source, Config.rocks[i].item) >= 1 then
             local rewardAmount = 0
-            for j=1, xPlayer.getInventoryItem(Config.rocks[i].item).count do
+            for j=1, HasItem(Config.rocks[i].item) do
                 rewardAmount = rewardAmount + math.random(Config.rocks[i].price[1], Config.rocks[i].price[2])
             end
             if rewardAmount > 0 then
-                xPlayer.addMoney(rewardAmount)
+                AddMoney(source, 'money', rewardAmount)
                 TriggerClientEvent('wasabi_mining:notify', source, Strings.sold_for, (Strings.sold_for_desc):format(xPlayer.getInventoryItem(Config.rocks[i].item).count, xPlayer.getInventoryItem(Config.rocks[i].item).label, addCommas(rewardAmount)), 'success')
-                xPlayer.removeInventoryItem(Config.rocks[i].item, xPlayer.getInventoryItem(Config.rocks[i].item).count)
+                RemoveItem(source, Config.rocks[i].item, HasItem(Config.rocks[i].item))
             end
         end
     end
 end)
 
-RegisterNetEvent('wasabi_mining:axeBroke', function()
+RegisterServerEvent('wasabi_mining:axeBroke')
+AddEventHandler('wasabi_mining:axeBroke', function()
     local xPlayer = ESX.GetPlayerFromId(source)
-    local xItem = xPlayer.getInventoryItem('pickaxe')
-    if xItem.count >= 1 then
-        xPlayer.removeInventoryItem('pickaxe', 1)
+    if HasItem(source, 'pickaxe') >= 1 then
+        RemoveItem(source, 'pickaxe', 1)
     else
         Wait(2000)
         xPlayer.kick(Strings.kicked)
